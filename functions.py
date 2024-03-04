6f6d5972b2b95dca64fcf184d85b56ad8a1c50e4
Alexandre Stakonski <115736059+stakewalker@users.noreply.github.com>
2024-03-04 17:49:30 -0300

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,30 @@
+import os
+from dotenv import load_dotenv
+#import requests
+import pycoingecko
+
+load_dotenv()
+
+def get_sentiment(token):
+    pass
+
+def get_socialmedia(token):
+    pass
+
+def make_ta(token):
+    pass
+
+def get_news_score(token):
+    pass
+
+def get_exchange_score(token):
+    pass
+
+def get_market_score(token):
+    pass
+
+def join_scores():
+    pass
+
+def update_ranking():
+    pass
