f336b46fdba2db48bab25e0c00d6379ee93b016a
Alexandre Stakonski <115736059+stakewalker@users.noreply.github.com>
2024-03-05 08:19:46 -0300

    Create classes.py

---
+++ classes.py
@@ -0,0 +1,23 @@
+class Token:
+    def __init__(self, symbol):
+        self.symbol = symbol
+        self.id = coingecko_data[symbol]
+        def self.scores = {'sentiment': get_sentiment(symbol),
+                       'socialmedia': get_socialmedia(symbol),
+                       'ta': make_technical_analysis(symbol),
+                       'news': get_news_score(symbol),
+                       'exchange': get_exchange_score(symbol)
+                       }
+        # join ranking
+        # update ranking
+
+class Ranking:
+    def __init__(self):
+        # check for previous file
+        pass
+    def update(self):
+        # rank by scores
+    def new(self, token):
+        # add a new token to the ranking
+    def remove(self, token):
+        # remove token from the list
