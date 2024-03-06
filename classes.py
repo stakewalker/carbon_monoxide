de9a16cc79bbdf25dfc270a57df9f0c4caf1f35d
Alexandre Stakonski <115736059+stakewalker@users.noreply.github.com>
2024-03-06 16:56:51 -0300

    Update classes.py

--- classes.py
+++ classes.py
@@ -1,3 +1,6 @@
+import os
+import json
+
 class Token:
     def __init__(self, symbol):
         self.symbol = symbol
@@ -8,19 +11,37 @@ class Token:
                        'news': get_news_score(symbol),
                        'exchange': get_exchange_score(symbol)
                        }
-        # join ranking
-        # update ranking
+    def join_ranking(self, ranking):
+        self.ranking = ranking
+        ranking.add_token(self)
+        
+    def update_ranking(self):
+        self.ranking.update_token(self)
+
 
 class Ranking:
     def __init__(self):
+        self.filename = 'rank.json'
         # check for previous file
-        pass
+        if not os.path.exists(self.filename):
+            with open(self.filename, 'w') as f:
+                json.dump({}, f)
+        with open(self.filename, 'r') as f:
+            self.rankings = json.load(f)
+            
     def update(self):
         # rank by scores
-        pass
-    def new(self, token):
+        self.rankings = dict(sorted(self.rankings.items(), key=lambda item: item[1], reverse=True))
+        with open(self.filename, 'w') as f:
+            json.dump(self.rankings, f)
+
+    def new(self, token, score):
         # add a new token to the ranking
-        pass
+        self.rankings[token] = score
+        self.update()
+
     def remove(self, token):
         # remove token from the list
-        pass
+        if token in self.rankings:
+            del self.rankings[token]
+            self.update()
