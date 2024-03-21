d48d4d997cdb52aabe6504e1022f23ceaca443e1
Alexandre Stakonski <115736059+stakewalker@users.noreply.github.com>
2024-03-21 00:11:46 +0000

    Return sentiment from news title and part of content

---
+++ sentiment_from_news.py
@@ -0,0 +1,27 @@
+import requests
+from textblob import TextBlob
+import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
+news_data = requests.get('https://min-api.cryptocompare.com/data/v2/news/?categories=BTC,ETH&excludeCategories=Sponsored').json()['Data']
+
+# Download and initialize
+nltk.download('vader_lexicon')
+sia = SentimentIntensityAnalyzer()
+
+def get_sentiment(text):
+    # Use the polarity_scores method to get the sentiment metrics
+    sentiment = sia.polarity_scores(text)
+    
+    # -1 (most extreme negative) and +1 (most extreme positive)
+    return sentiment['compound']
+
+counter = 0
+for i in news_data:
+    print({
+        i['title'],
+        get_sentiment(i['title']),
+        i['body'],
+        get_sentiment(i['body'])
+        })
+    counter += 1
