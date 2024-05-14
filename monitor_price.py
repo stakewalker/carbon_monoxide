c1c51a75104336dba62df03d366483efd04ae2ea
Alexandre Stakonski <115736059+stakewalker@users.noreply.github.com>
2024-05-14 12:05:20 -0300

    Create monitor_price.py
    More functionalities coming soon...

---
+++ monitor_price.py
@@ -0,0 +1,6 @@
+import requests
+
+binance_tokens = [symbol['symbol'][:-4] for symbol in requests.get('https://api.binance.com/api/v3/exchangeInfo').json()['symbols'] if symbol['symbol'].endswith('USDT')]
+def spot_price(token):
+    if token in binance_tokens:
+        return float(requests.get(f'https://api.binance.com/api/v3/ticker/price?symbol={token.upper()}USDT').json()['price'])
